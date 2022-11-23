4eb58d75707848a3526f5e3f87e73a61437c5721
Exlord <adeli.farhad@gmail.com>
2022-11-23 13:18:51 +0330

    done task 4

--- nest_prisma/prisma/migrations/1663877813247-CinemaSystem.ts
+++ nest_prisma/prisma/migrations/1663877813247-CinemaSystem.ts
@@ -1,4 +1,7 @@
 import { Migration } from '../cli/migration';
+import { PrismaService } from '../../src/prisma/prisma.service';
+
+const prisma = new PrismaService();
 
 export default class implements Migration {
   async up() {
@@ -32,10 +35,60 @@ export default class implements Migration {
      * As a cinema owner I dont want to configure the seating for every show
      */
 
-    throw new Error('TODO: implement migration in task 4');
+    try {
+      await prisma.$queryRaw`CREATE TABLE "ShowRoom" (
+                                                         "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                                         "name" TEXT NOT NULL
+                             );`;
+      await prisma.$queryRaw`CREATE TABLE "movies" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "ticketPrice" INTEGER NOT NULL
+);`;
+      await prisma.$queryRaw`CREATE TABLE "MovieTime" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "start" DATETIME NOT NULL,
+    "movieId" INTEGER NOT NULL,
+    "showRoomId" INTEGER NOT NULL,
+    CONSTRAINT "MovieTime_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "movies" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "MovieTime_showRoomId_fkey" FOREIGN KEY ("showRoomId") REFERENCES "ShowRoom" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);`;
+      await prisma.$queryRaw`CREATE TABLE "CinemaSeat" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "seatTypeId" INTEGER NOT NULL,
+    "showRoomId" INTEGER NOT NULL,
+    "locationX" INTEGER NOT NULL,
+    "locationY" INTEGER NOT NULL,
+    CONSTRAINT "CinemaSeat_seatTypeId_fkey" FOREIGN KEY ("seatTypeId") REFERENCES "CinemaSeatType" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "CinemaSeat_showRoomId_fkey" FOREIGN KEY ("showRoomId") REFERENCES "ShowRoom" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);`;
+      await prisma.$queryRaw`CREATE TABLE "CinemaSeatType" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "premiumPercentage" INTEGER NOT NULL
+);`;
+      await prisma.$queryRaw`CREATE TABLE "BookedSeat" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "seatId" INTEGER NOT NULL,
+    "timeId" INTEGER NOT NULL,
+    CONSTRAINT "BookedSeat_seatId_fkey" FOREIGN KEY ("seatId") REFERENCES "CinemaSeat" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "BookedSeat_timeId_fkey" FOREIGN KEY ("timeId") REFERENCES "MovieTime" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);`;
+
+      // automatic rollback on error
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await prisma.$disconnect();
+    }
   }
 
   async down() {
-    // do nothing
+    await prisma.$queryRaw`DROP TABLE "ShowRoom"`;
+    await prisma.$queryRaw`DROP TABLE "movies"`;
+    await prisma.$queryRaw`DROP TABLE "MovieTime"`;
+    await prisma.$queryRaw`DROP TABLE "CinemaSeat"`;
+    await prisma.$queryRaw`DROP TABLE "CinemaSeatType"`;
+    await prisma.$queryRaw`DROP TABLE "BookedSeat"`;
   }
 }
